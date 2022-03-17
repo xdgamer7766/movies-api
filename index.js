6d04657d2c9904089095975b2f72cfb304765a6e
Polgár Benedek <oszpolgarbenedek@gmail.com>
2022-03-17 19:09:20 +0100

    first_commit

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require("express")
-const reptiles = require("./routes/reptile.js")
-const birds = require("./routes/bird.js")
-const amphibians = require("./routes/amphibian.js")
+
 
 const app = express()
 
-app.use("/reptiles",reptiles)
-app.use("/birds",birds)
-app.use("/amphibians",amphibians)
 app.get("/api",(req,res) => {
     res.send("api version: 1.0.0")
 })
